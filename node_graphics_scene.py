c9efd971562a4b45ef02bf17113b98fee157fbe2
christopherkopic <christopherkopic@web.de>
2019-06-14 23:02:08 +0200

    Added darker grid

--- node_graphics_scene.py
+++ node_graphics_scene.py
@@ -8,13 +8,17 @@ class QCKGraphicsScene(QGraphicsScene):
         super().__init__(parent)
 
         #settings
-        self.grid_size = 20
+        self.grid_size = 30
+        self.grid_squares = 4
 
         self._color_background = QColor("#393939")
         self._color_light = QColor("#2f2f2f")
+        self._color_dark = QColor("#292929")
 
         self.penLight = QPen(self._color_light)
-        self.penLight.setWidth(1)
+        self.penLight.setWidth(2)
+        self.penDark = QPen(self._color_dark)
+        self.penDark.setWidth(3)
 
         self.scene_width, self.scene_height = 64000, 64000
         self.setSceneRect(-self.scene_width//2, -self.scene_height//2, self.scene_width, self.scene_height) # // equals divide and round to int
@@ -34,14 +38,18 @@ class QCKGraphicsScene(QGraphicsScene):
         first_top = top - (top % self.grid_size)
 
         # compute all lines to be draw
-        lines_light = []
+        lines_light, lines_dark = [], []
         for x in range(first_left, right, self.grid_size):
-            lines_light.append(QLine(x, top, x, bottom))
+            if (x % (self.grid_size * self.grid_squares) != 0): lines_light.append(QLine(x, top, x, bottom))
+            else: lines_dark.append(QLine(x, top, x, bottom))
 
         for y in range(first_top, bottom, self.grid_size):
-            lines_light.append(QLine(left, y, right, y))
-
+            if (y % (self.grid_size * self.grid_squares) != 0): lines_light.append(QLine(left, y, right, y))
+            else: lines_dark.append(QLine(left, y, right, y))
 
         # draw lines
         painter.setPen(self.penLight)
         painter.drawLines(*lines_light)  # * equals pass array as individual elements
+
+        painter.setPen(self.penDark)
+        painter.drawLines(*lines_dark)
