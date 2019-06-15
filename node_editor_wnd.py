9a7e4c89f4d8d6ce1d130ce6e0589640bf9e3043
christopherkopic <christopherkopic@web.de>
2019-06-15 12:40:58 +0200

    Buncha Elements

--- node_editor_wnd.py
+++ node_editor_wnd.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 from node_graphics_scene import QCKGraphicsScene
 
@@ -25,3 +27,32 @@ class NodeEditorWnd(QWidget):
 
         self.setWindowTitle("Node Editor")
         self.show()
+
+        self.addDebugContent()
+
+    def addDebugContent(self):
+        greenBrush = QBrush(Qt.green)
+        outlinePen = QPen(Qt.black)
+        outlinePen.setWidth(3)
+
+        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
+        rect.setFlag(QGraphicsItem.ItemIsSelectable)
+
+        text = self.grScene.addText("Hello There!\nGeneral Kenobi!", QFont("Ubuntu"))
+        text.setFlag(QGraphicsItem.ItemIsSelectable)
+        text.setFlag(QGraphicsItem.ItemIsMovable)
+        text.setDefaultTextColor(QColor.fromRgbF(1.0,1.0,1.0))
+
+        widget1 = QPushButton("Hello There")
+        proxy1 = self.grScene.addWidget(widget1)
+        proxy1.setPos(0, 60)
+
+        # Somewhat broken
+        # widget2 = QTextEdit()
+        # proxy2 = self.grScene.addWidget(widget2)
+        # proxy2.setFlag(QGraphicsItem.ItemIsMovable)
+
+        line = self.grScene.addLine(-200,-200,400,-100, outlinePen)
+        line.setFlag(QGraphicsItem.ItemIsMovable)
+        line.setFlag(QGraphicsItem.ItemIsSelectable)
