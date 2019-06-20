4d68fd0e25ef275d795f67ff8e4dcf1663312365
christopherkopic <christopherkopic@web.de>
2019-06-20 22:03:40 +0200

    Static Edges between Nodes

--- node_edge.py
+++ node_edge.py
@@ -11,4 +11,32 @@ class Edge():
         self.end_socket = end_socket
 
         self.grEdge = QCKGraphicsEdgeDirect(self) if type==EDGE_TYPE_DIRECT else QCKGraphicsEdgeBezier(self)
+
+        self.updatePositions()
         self.scene.grScene.addItem(self.grEdge)
+
+    def updatePositions(self):
+        source_pos = self.start_socket.getSocketPosition()
+        source_pos[0] += self.start_socket.node.grNode.pos().x()
+        source_pos[1] += self.start_socket.node.grNode.pos().y()
+        self.grEdge.setSource(*source_pos)
+        if self.end_socket is not None:
+            end_pos = self.end_socket.getSocketPosition()
+            end_pos[0] += self.end_socket.node.grNode.pos().x()
+            end_pos[1] += self.end_socket.node.grNode.pos().y()
+            self.grEdge.setDestination(*end_pos)
+        self.grEdge.update()
+
+    def remove_from_sockets(self):
+        if self.start_socket is not None:
+            self.start_socket.edge = None
+        if self.end_socket is not None:
+            self.end_socket.edge = None
+        self.end_socket = None
+        self.start_socket = None
+
+    def remove(self):
+        self.remove_from_sockets()
+        self.scene.grScene.removeItem(self.grEdge)
+        self.grEdge = None
+        self.scene.removeEdge(self)
--- node_socket.py
+++ node_socket.py
@@ -9,7 +9,15 @@ class Socket():
     def __init__(self, node, index=0, position=LEFT_TOP):
         self.node = node
         self.index = index
-        self.position = LEFT_TOP
+        self.position = position
 
         self.grSocket = QCKGraphicsSocket(self.node.grNode)
         self.grSocket.setPos(*self.node.getSocketPosition(index, position))
+
+        self.edge = None
+
+    def getSocketPosition(self):
+        return self.node.getSocketPosition(self.index, self.position)
+
+    def setConnectedEdge(self, edge=None):
+        self.edge = edge
